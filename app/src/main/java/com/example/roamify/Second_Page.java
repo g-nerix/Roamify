1a84d804a794ca945f0a241cda858d3b7aedb12e
AbhimanyuBhatnagar <abhimanyu20273@iiitd.ac.in>
2023-05-08 05:05:33 +0530

    Second page links

--- app/src/main/java/com/example/roamify/Second_Page.java
+++ app/src/main/java/com/example/roamify/Second_Page.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -41,6 +42,7 @@ public class Second_Page extends AppCompatActivity
     ImageButton search_button;
     private String country;
     private double latitude, longitude;
+    private ImageView view1,view2,view3,view4,view5,view6;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -86,6 +88,80 @@ public class Second_Page extends AppCompatActivity
             @Override
             public boolean onQueryTextChange(String newText) {
                 return false;
+            }
+        });
+        setbuttons();
+    }
+
+    private void setbuttons()
+    {
+        ImageView view1 = findViewById(R.id.img);
+        ImageView view2 = findViewById(R.id.img_1);
+        ImageView view3 = findViewById(R.id.img_2);
+        ImageView view4 = findViewById(R.id.image4);
+        ImageView view5 = findViewById(R.id.image5);
+        ImageView view6 = findViewById(R.id.image6);
+        ConnectivityManager connectivity_manager = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo net_info = connectivity_manager.getActiveNetworkInfo();
+        boolean check_connection = net_info != null && net_info.isConnected();
+        if(check_connection != true)
+        {
+            Toast.makeText(getApplicationContext(),"Connection error",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        view1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String API_link = String.format("https://api.foursquare.com/v3/places/search?near=%s","Delhi");
+                Main_search myTask=new Main_search(getApplicationContext(),API_link,"Delhi");
+                myTask.execute();
+            }
+        });
+        view2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String API_link = String.format("https://api.foursquare.com/v3/places/search?near=%s","Bangalore");
+                Main_search myTask=new Main_search(getApplicationContext(),API_link,"Bangalore");
+                myTask.execute();
+            }
+        });
+        view3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String API_link = String.format("https://api.foursquare.com/v3/places/search?near=%s","Hyderabad");
+                Main_search myTask=new Main_search(getApplicationContext(),API_link,"Hyderabad");
+                myTask.execute();
+            }
+        });
+        view4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String API_link = String.format("https://api.foursquare.com/v3/places/search?near=%s","Mumbai");
+                Main_search myTask=new Main_search(getApplicationContext(),API_link,"Mumbai");
+                myTask.execute();
+            }
+        });
+        view5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String API_link = String.format("https://api.foursquare.com/v3/places/search?near=%s","Paris");
+                Main_search myTask=new Main_search(getApplicationContext(),API_link,"Paris");
+                myTask.execute();
+            }
+        });
+        view6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String API_link = String.format("https://api.foursquare.com/v3/places/search?near=%s","New York");
+                Main_search myTask=new Main_search(getApplicationContext(),API_link,"New York");
+                myTask.execute();
             }
         });
     }

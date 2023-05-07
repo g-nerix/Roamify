bd1f4345e76c158bb049436b35c146e2ed5a7e75
AbhimanyuBhatnagar <abhimanyu20273@iiitd.ac.in>
2023-05-07 14:02:56 +0530

    API class and name coord class added

--- app/src/main/java/com/example/roamify/MainActivity.java
+++ app/src/main/java/com/example/roamify/MainActivity.java
@@ -6,8 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    String Location_string;
+    EditText Location_edit_text;
+    final String API_key = "5ae2e3f221c38a28845f05b6a91332a87694c022a5bfe17fff46a616";
+    final String original_API_link = "https://api.opentripmap.com/0.1/en/places/";
+    Button search_button;
+    private String country;
+    private double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +26,11 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
+        new Handler().postDelayed(new Runnable()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 Intent i = new Intent(MainActivity.this, HomeScreen.class);
                 startActivity(i);
                 finish();

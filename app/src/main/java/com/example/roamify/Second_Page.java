b6ba33bbc009379934914ed5d8f71d773b81088d
work <ayush20502@iiitd.ac.in>
2023-05-08 04:21:37 +0530

    Second Page UI update

--- app/src/main/java/com/example/roamify/Second_Page.java
+++ app/src/main/java/com/example/roamify/Second_Page.java
@@ -1,5 +1,6 @@
 package com.example.roamify;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,20 +46,15 @@ public class Second_Page extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_page);
-        set_button_listener();
-    }
-    public void set_button_listener()
-    {
-        search_button = (ImageButton)  findViewById(R.id.Search_button);
-        search_button.setOnClickListener(new View.OnClickListener() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        SearchView searchView = findViewById(R.id.location);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View view)
-            {
-                Location_edit_text = (EditText) findViewById(R.id.location);
-                Location_string = Location_edit_text.getText().toString();
+            public boolean onQueryTextSubmit(String query) {
                 int flag = 0;
-                for (int i = 0; i < Location_string.length(); i++) {
-                    if (Character.isLetter(Location_string.charAt(i)) == false)
+                for (int i = 0; i < query.length(); i++) {
+                    if (Character.isLetter(query.charAt(i)) == false)
                     {
                         flag = -1;
                         break;
@@ -70,8 +67,8 @@ public class Second_Page extends AppCompatActivity
                     boolean check_connection = net_info != null && net_info.isConnected();
                     if(check_connection == true)
                     {
-                        String API_link = String.format("https://api.foursquare.com/v3/places/search?near=%s",Location_string);
-                        Main_search myTask=new Main_search(getApplicationContext(),API_link,Location_string);
+                        String API_link = String.format("https://api.foursquare.com/v3/places/search?near=%s",query);
+                        Main_search myTask=new Main_search(getApplicationContext(),API_link,query);
                         myTask.execute();
                     }
                     else
@@ -83,7 +80,53 @@ public class Second_Page extends AppCompatActivity
                 {
                     Toast.makeText(getApplicationContext(),"Invalid input",Toast.LENGTH_SHORT).show();
                 }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
     }
+//    public void set_button_listener()
+//    {
+//        search_button = (ImageButton)  findViewById(R.id.Search_button);
+//        search_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view)
+//            {
+//                Location_edit_text = (EditText) findViewById(R.id.location);
+//                Location_string = Location_edit_text.getText().toString();
+//                int flag = 0;
+//                for (int i = 0; i < Location_string.length(); i++) {
+//                    if (Character.isLetter(Location_string.charAt(i)) == false)
+//                    {
+//                        flag = -1;
+//                        break;
+//                    }
+//                }
+//                if(flag == 0)
+//                {
+//                    ConnectivityManager connectivity_manager = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+//                    NetworkInfo net_info = connectivity_manager.getActiveNetworkInfo();
+//                    boolean check_connection = net_info != null && net_info.isConnected();
+//                    if(check_connection == true)
+//                    {
+//                        String API_link = String.format("https://api.foursquare.com/v3/places/search?near=%s",Location_string);
+//                        Main_search myTask=new Main_search(getApplicationContext(),API_link,Location_string);
+//                        myTask.execute();
+//                    }
+//                    else
+//                    {
+//                        Toast.makeText(getApplicationContext(),"Connection error",Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//                if(flag == -1)
+//                {
+//                    Toast.makeText(getApplicationContext(),"Invalid input",Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//    }
 }

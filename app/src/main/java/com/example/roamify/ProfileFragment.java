7b43ad92966ee8af08e564c8148ae2b09344be0a
sarthak20574 <sarthak20574@iiitd.ac.in>
2023-05-08 06:29:39 +0530

    I hate UI

--- app/src/main/java/com/example/roamify/ProfileFragment.java
+++ app/src/main/java/com/example/roamify/ProfileFragment.java
@@ -28,7 +28,7 @@ public class ProfileFragment extends Fragment {
     FirebaseUser user;
 
     DatabaseReference mDatabase;
-    TextView name, email, age;
+    TextView name,name1, email,email1, age;
 
     Button logout, delete;
 
@@ -44,9 +44,12 @@ public class ProfileFragment extends Fragment {
 
         logout= view.findViewById(R.id.logoutBtn);
         name= view.findViewById(R.id.Name);
+        name1= view.findViewById(R.id.Name_1);
+
         age= view.findViewById(R.id.Age);
         email= view.findViewById(R.id.emailContainer);
-        //delete= view.findViewById(R.id.Delete);
+        email1= view.findViewById(R.id.email_1);
+        delete= view.findViewById(R.id.Delete);
 
         if( user==null){
 //                    profile.setOnClickListener(new View.OnClickListener() {
@@ -67,9 +70,11 @@ public class ProfileFragment extends Fragment {
             getActivity().finish();
         }
         email.setText("Email: "+user.getEmail());
+        email1.setText("Email: "+user.getEmail());
 
         String[] x= user.getDisplayName().split("#");
         name.setText("Name: "+x[0]);
+        name1.setText("Name: "+x[0]);
         age.setText("Age: "+x[1]);
 
         logout.setOnClickListener(new View.OnClickListener() {
@@ -84,40 +89,40 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-//        delete.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-//                builder.setTitle("Delete Account")
-//                        .setMessage("Are you sure you want to delete your account?")
-//                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialogInterface, int i) {
-//                                // Delete the user's account
-//                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//                                user.delete()
-//                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                            @Override
-//                                            public void onComplete(@NonNull Task<Void> task) {
-//                                                if (task.isSuccessful()) {
-//                                                    // Delete the user's data from the Realtime Database
-//                                                    mDatabase.child("users").child(user.getUid()).removeValue();
-//                                                    // Sign out and return to the login screen
-//                                                    FirebaseAuth.getInstance().signOut();
-//                                                    Intent intent = new Intent(getActivity(), Login.class);
-//                                                    startActivity(intent);
-//                                                    getActivity().finish();
-//                                                } else {
-//                                                    Toast.makeText(getContext(), "Failed to delete account", Toast.LENGTH_SHORT).show();
-//                                                }
-//                                            }
-//                                        });
-//                            }
-//                        })
-//                        .setNegativeButton("No", null)
-//                        .show();
-//            }
-//        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Delete Account")
+                        .setMessage("Are you sure you want to delete your account?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // Delete the user's account
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                user.delete()
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    // Delete the user's data from the Realtime Database
+                                                    mDatabase.child("users").child(user.getUid()).removeValue();
+                                                    // Sign out and return to the login screen
+                                                    FirebaseAuth.getInstance().signOut();
+                                                    Intent intent = new Intent(getActivity(), Login.class);
+                                                    startActivity(intent);
+                                                    getActivity().finish();
+                                                } else {
+                                                    Toast.makeText(getContext(), "Failed to delete account", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+            }
+        });
 
 
         return view;

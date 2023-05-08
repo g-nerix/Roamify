ae0a36b85b34cfaf2dcf761d6c28083b4b210145
AbhimanyuBhatnagar <abhimanyu20273@iiitd.ac.in>
2023-05-08 06:57:29 +0530

    Map fix

--- app/src/main/java/com/example/roamify/HomeScreen.java
+++ app/src/main/java/com/example/roamify/HomeScreen.java
@@ -39,7 +39,7 @@ public class HomeScreen extends AppCompatActivity implements ExploreFragment.But
     SearchFragment searchFragment = new SearchFragment();
     private Intent value_received_from_previous_activity;
     public String place;
-    public double lon = 77.27,lat = 28.54;
+    public double lon = 77.2732,lat = 28.5459;
     public String name = "IIIsTD";
     public ArrayList<String> checked_name_list = new ArrayList<>();
     public ArrayList<Integer> checked_price_list = new ArrayList<>();
